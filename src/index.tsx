a4ba992f62e005362080facd1f6810dea9489afa
Dheeraj Agrawal <dheeraj.agrawal@industrieit.com>
2019-10-30 18:59:31 +1100

    moving notification to its own service

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import * as serviceWorker from './registerServiceWorker';
 export function tasks(location: Location) {
   return location.href.indexOf(`${location.origin}/tasks`) !== -1;
 }
-
+export function notifications(location: Location) {
+  return location.href.indexOf(`${location.origin}/notifications`) !== -1;
+}
 export const navbar = (location: Location) => true;
 
-registerApplication('navbar', () => SystemJS.import('//localhost:3000/static/js/bundle.js'), () => true);
-// registerApplication('tasks', () => SystemJS.impo rt('@portal/tasks'), tasks);
+registerApplication('navbar', () => SystemJS.import('//localhost:3000/static/js/bundle.js'), navbar);
+registerApplication('tasks', () => SystemJS.import('//localhost:3001/static/js/bundle.js'), tasks);
+registerApplication('notifications', () => SystemJS.import('//localhost:3003/static/js/bundle.js'), notifications);
 start();
 serviceWorker.unregister();
 
