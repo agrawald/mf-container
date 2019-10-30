12481cf9fe28c9b9c2d345b211a15d357a23da32
Dheeraj Agrawal <dheeraj.agrawal@industrieit.com>
2019-10-30 22:20:08 +1100

    Changing the libarayTarget to UMD

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,20 @@ export function notifications(location: Location) {
 }
 export const navbar = (location: Location) => true;
 
-registerApplication('navbar', () => SystemJS.import('//localhost:3000/static/js/bundle.js'), navbar);
-registerApplication('tasks', () => SystemJS.import('//localhost:3001/static/js/bundle.js'), tasks);
-registerApplication('notifications', () => SystemJS.import('//localhost:3003/static/js/bundle.js'), notifications);
+registerApplication(
+  'navbar',
+  () => SystemJS.import('//' + process.env.REACT_APP_NAVBAR_URL + '/static/js/bundle.js'),
+  navbar
+);
+registerApplication(
+  'tasks',
+  () => SystemJS.import('//' + process.env.REACT_APP_TASKS_URL + '/static/js/bundle.js'),
+  tasks
+);
+registerApplication(
+  'notifications',
+  () => SystemJS.import('//' + process.env.REACT_APP_NOTIFICATION_URL + '/static/js/bundle.js'),
+  notifications
+);
 start();
 serviceWorker.unregister();
-
